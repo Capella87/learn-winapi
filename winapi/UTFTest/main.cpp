a10471cdf29a1ff865f3d697014b1c7aaba74103
Capella87 <contact@capelladev.com>
2024-02-10 23:58:22 +0900

    [UTFTest] Show UTF-8 code

--- winapi/UTFTest/main.cpp
+++ winapi/UTFTest/main.cpp
@@ -8,11 +8,51 @@
 // disable warning
 #pragma warning(disable:4996)
 
+typedef enum BASEENUM
+{
+    HEX,
+    BIN,
+    DEC,
+    OCTAL
+} baseenum;
+
+const char* get_binary(unsigned char* ch)
+{
+    char rt[9] = "00000000";
+    char num = ch[0];
+    char* cursor = &rt[7];
+
+    while (num && cursor > rt)
+    {
+        *cursor = (num % 2) + '0';
+        cursor--;
+        num /= 2;
+    }
+
+    return rt;
+}
+
+unsigned char get_utf8_code(const char* ch, size_t len, baseenum mode)
+{
+    unsigned char chr = 0;
+    // Disassemble
+    printf("0x");
+    for (size_t i = 0; i < len; i++)
+    {
+        chr = (unsigned char)ch[i];
+        printf("%x ", chr);
+    }
+    putchar('\n');
+
+    return (unsigned char)0;
+}
+
 int main(int argc, char** argv)
 {
-    const char* latest_emoji = "🛜";
+    const char* latest_emoji = "🪫";
     printf("%s\n", latest_emoji);
     printf("Length: %llu\n", strlen(latest_emoji));
+    get_utf8_code(latest_emoji, strlen(latest_emoji), HEX);
 
     DWORD dwVersion = 0;
     DWORD dwMajorVersion = 0;
@@ -42,5 +82,6 @@ int main(int argc, char** argv)
     {
         printf("Yes! you're using Windows 10 or later\n");
     }
+
     return 0;
 }
